ee13a6ec5154bffc7004940063ef14d33f23ca4c
Isac <isac08270@gmail.com>
2021-12-20 16:48:09 +1100

    added loading screen

--- src/main.js
+++ src/main.js
@@ -2,6 +2,29 @@
 
 import { Game } from "./game.js"
 
+let cvs = document.getElementById("cvs")
+let ctx = cvs.getContext("2d")
+let dotNum = 0
+let loadingText = ""
+let fontWidth = 0
+let fontHeight = 20
+ctx.font = `${fontHeight}px Roboto Condensed`
+let loadingLoop = setInterval(showLoadingScreen, 1000)
+
+function showLoadingScreen() {
+    dotNum++
+    ctx.clearRect(0, 0, cvs.width, cvs.height)
+    loadingText = `Loading${dotNum*"."}`
+    fontWidth = ctx.measureText(loadingText).width
+    ctx.fillText(loadingText, (cvs.width-fontWidth)/2, (cvs.height-fontHeight)/2)
+    if (dotNum == 3) {
+        dotNum = 0
+    }
+}
+
+
 window.onload = function() {
+    clearInterval(loadingLoop)
+    ctx.clearRect(0, 0, cvs.width, cvs.height)
     new Game().start()
 }
